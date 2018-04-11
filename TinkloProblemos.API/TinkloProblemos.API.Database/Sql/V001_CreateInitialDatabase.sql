299fd9d60f3e68c4d49c5fb3ec24478ab531bb21
VSW\tomas.valiunas <tomas.valiunas@visma.com>
2018-04-11 17:04:46 +0300

    Fixed database script to use identity user instead of regular user

--- TinkloProblemos.API/TinkloProblemos.API.Database/Sql/V001_CreateInitialDatabase.sql
+++ TinkloProblemos.API/TinkloProblemos.API.Database/Sql/V001_CreateInitialDatabase.sql
@@ -1,21 +1,112 @@
 use bakalaurinis;
+CREATE TABLE Users(
+	Id CHAR(38) NOT NULL,
+	FirstName VARCHAR(128) NULL,
+	LastName VARCHAR(128) NULL,
+	UserName VARCHAR(64) NOT NULL,
+	NormalizedUserName VARCHAR(64) NOT NULL,
+	Email VARCHAR(64) NOT NULL,
+	NormalizedEmail VARCHAR(64) NOT NULL,
+	EmailConfirmed bit NOT NULL,
+	PasswordHash VARCHAR(128) NULL,
+	PhoneNumber VARCHAR(32) NULL,
+	PhoneNumberConfirmed bit NOT NULL,
+	PhotoUrl VARCHAR(128) NULL,
+	Address VARCHAR(256) NULL,
+	ConcurrencyStamp VARCHAR(128) NULL,
+	SecurityStamp VARCHAR(128) NULL,
+	RegistrationDate datetime NULL,
+	LastLoginDate datetime NULL,
+	LockoutEnabled bit NOT NULL,
+	LockoutEndDateTimeUtc datetime NULL,
+	TwoFactorEnabled bit NOT NULL,
+	AccessFailedCount int NOT NULL,
+   CONSTRAINT PK_IdentityUser PRIMARY KEY (Id)
+);
+
+CREATE VIEW GetTotalNumberOfUsers
+AS
+SELECT COUNT(*) AS TotalNumberOfUsers
+FROM Users;
+
+CREATE TABLE Roles(
+	Id CHAR(38) NOT NULL,
+	Name VARCHAR(32) NOT NULL,
+	NormalizedName VARCHAR(32) NOT NULL,
+	ConcurrencyStamp VARCHAR(128) NULL,
+	CONSTRAINT PK_Roles PRIMARY KEY (Id)
+);
+
+CREATE TABLE UsersClaims(
+	Id CHAR(38) NOT NULL,
+	UserId CHAR(38) NOT NULL,
+	ClaimType VARCHAR(65) NOT NULL,
+	ClaimValue VARCHAR(65) NOT NULL,
+	CONSTRAINT PK_UsersClaims PRIMARY KEY (Id)
+);
+
+CREATE TABLE UsersLogins(
+	LoginProvider VARCHAR(32) NOT NULL,
+	ProviderKey VARCHAR(64) NOT NULL,
+	UserId CHAR(38) NOT NULL,
+	ProviderDisplayName VARCHAR(32) NOT NULL,
+	CONSTRAINT PK_UsersLogins PRIMARY KEY (LoginProvider, ProviderKey)
+);
+
+CREATE TABLE UsersRoles(
+	UserId CHAR(38) NOT NULL,
+	RoleId CHAR(38) NOT NULL,
+ CONSTRAINT PK_UsersRoles PRIMARY KEY (UserId, RoleId)
+ );
+
+ALTER TABLE Users MODIFY COLUMN EmailConfirmed bit  NOT NULL DEFAULT 0;
+ALTER TABLE Users MODIFY COLUMN PhoneNumberConfirmed bit  NOT NULL DEFAULT 0;
+ALTER TABLE Users MODIFY COLUMN LockoutEnabled bit  NOT NULL DEFAULT 0;
+ALTER TABLE Users MODIFY COLUMN TwoFactorEnabled bit  NOT NULL DEFAULT 0;
+ALTER TABLE Users MODIFY COLUMN AccessFailedCount int  NOT NULL DEFAULT 0;
+
+ALTER TABLE UsersClaims ADD CONSTRAINT FK_UsersClaims_Users FOREIGN KEY(UserId) REFERENCES Users(Id)
+ON UPDATE CASCADE
+ON DELETE CASCADE;
+
+ALTER TABLE UsersLogins ADD CONSTRAINT FK_UsersLogins_Users FOREIGN KEY(UserId)
+REFERENCES Users (Id)
+ON UPDATE CASCADE
+ON DELETE CASCADE;
+
+ALTER TABLE UsersRoles  ADD CONSTRAINT FK_UsersRoles_Roles FOREIGN KEY(RoleId)
+REFERENCES Roles (Id)
+ON UPDATE CASCADE
+ON DELETE CASCADE;
+
+ALTER TABLE UsersRoles  ADD  CONSTRAINT FK_UsersRoles_Users FOREIGN KEY(UserId)
+REFERENCES Users (Id)
+ON UPDATE CASCADE
+ON DELETE CASCADE;
+
+INSERT INTO Roles(Id, Name, NormalizedName, ConcurrencyStamp) VALUES ('642A2EBD-1B2F-4321-B294-028C3A27E0A2', 'User', 'USER', '1FE5B345-A5E0-496E-8956-B48D21AA37CC');
+INSERT INTO Roles(Id, Name, NormalizedName, ConcurrencyStamp) VALUES ('D2A35AEC-402A-4BE6-9D7F-682C0B5D3FEF', 'Administrator', 'ADMINISTRATOR', 'BD480BB1-7D73-4393-AA09-51B11AAC949E');
+
+INSERT INTO Users (Id, FirstName, LastName, UserName, NormalizedUserName, Email, NormalizedEmail, EmailConfirmed, PasswordHash, PhoneNumber, PhoneNumberConfirmed, PhotoUrl, Address, ConcurrencyStamp, SecurityStamp, RegistrationDate, LastLoginDate, LockoutEnabled, LockoutEndDateTimeUtc, TwoFactorEnabled, AccessFailedCount) VALUES ('810BAD28-CED4-427A-9A88-5A17682304DA', 'Tomas', 'Valiunas', 'admin@dadmin.com', 'ADMIN@ADMIN.com', 'admin@admin.com', 'ADMIN@ADMIN.com', 1, 'AQAAAAEAACcQAAAAEFKjcOnLHrAhew3z9YUjO/vGiExMDrNi54ZyUKACsW8rn1vDTleOUvJeNoEG3JU4fQ==', '+306981908600', 1, NULL, 'Kyprou 14, Agia Varvara', '70C14A94-36D7-4CF7-973E-0425742980A8', 'A1991EC9-C0F3-40A5-8457-61032D13B1CD', '2018-04-08', NULL, 0, NULL, 0, 0);
+INSERT INTO UsersRoles (UserId, RoleId) VALUES ('810BAD28-CED4-427A-9A88-5A17682304DA', '642A2EBD-1B2F-4321-B294-028C3A27E0A2');
+INSERT INTO UsersRoles (UserId, RoleId) VALUES ('810BAD28-CED4-427A-9A88-5A17682304DA', 'D2A35AEC-402A-4BE6-9D7F-682C0B5D3FEF');
+
 CREATE TABLE Category (Id int(10) NOT NULL AUTO_INCREMENT, Name varchar(255) NOT NULL, Description varchar(512), PRIMARY KEY (Id));
 CREATE TABLE Category_Problem (CategoryId int(10) NOT NULL, ProblemId int(10) NOT NULL, PRIMARY KEY (CategoryId, ProblemId));
-CREATE TABLE Comment (Id int(10) NOT NULL AUTO_INCREMENT, Text varchar(5000) NOT NULL, UserId int(10) NOT NULL, ProblemId int(10) NOT NULL, PRIMARY KEY (Id));
+CREATE TABLE Comment (Id int(10) NOT NULL AUTO_INCREMENT, Text varchar(5000) NOT NULL, UserId CHAR(38) NOT NULL, ProblemId int(10) NOT NULL, PRIMARY KEY (Id));
 CREATE TABLE InternetUser (Id int(10) NOT NULL AUTO_INCREMENT, FirstName varchar(255) NOT NULL, LastName int(10) NOT NULL, Description varchar(512), Location varchar(255), IpAddress varchar(255), PRIMARY KEY (Id));
 CREATE TABLE InternetUserDevice (Id int(10) NOT NULL AUTO_INCREMENT, Name varchar(255) NOT NULL, Manufacturer varchar(255), MacAddress varchar(255), WarrantyExpiration date, Description varchar(512), InternetUserId int(10) NOT NULL, PRIMARY KEY (Id));
-CREATE TABLE Problem (Id int(10) NOT NULL AUTO_INCREMENT, Name varchar(255) NOT NULL, Description varchar(9000) NOT NULL, Location varchar(512), Created date NOT NULL, AssignedUser int(10) NOT NULL, StatusId int(10) NOT NULL, InternetUserId int(10) NOT NULL, CONSTRAINT Id PRIMARY KEY (Id));
+CREATE TABLE Problem (Id int(10) NOT NULL AUTO_INCREMENT, Name varchar(255) NOT NULL, Description varchar(9000) NOT NULL, Location varchar(512), Created date NOT NULL, AssignedUser CHAR(38) NOT NULL, StatusId int(10) NOT NULL, InternetUserId int(10) NOT NULL, CONSTRAINT Id PRIMARY KEY (Id));
 CREATE TABLE Problem_Tag (TagId int(10) NOT NULL, ProblemId int(10) NOT NULL, PRIMARY KEY (TagId, ProblemId));
 CREATE TABLE Status (Id int(10) NOT NULL AUTO_INCREMENT, Name int(10) NOT NULL, PRIMARY KEY (Id));
 CREATE TABLE `Tag` (Id int(10) NOT NULL AUTO_INCREMENT, Name varchar(50) NOT NULL, PRIMARY KEY (Id));
-CREATE TABLE TimeSpent (Id int(10) NOT NULL AUTO_INCREMENT, HoursSpent decimal(2, 0) DEFAULT 0 NOT NULL, Description varchar(512), DateRecorded timestamp DEFAULT NOW() NOT NULL, UserId int(10) NOT NULL, ProblemId int(10) NOT NULL, PRIMARY KEY (Id));
-CREATE TABLE `User` (Id int(10) NOT NULL AUTO_INCREMENT, FirstName varchar(50) NOT NULL, LastName varchar(50) NOT NULL, Email varchar(255) NOT NULL UNIQUE, Password varchar(255) NOT NULL, PasswordSalt varchar(255) NOT NULL, Created timestamp NOT NULL, IsDisabled tinyint NOT NULL, PRIMARY KEY (Id));
-ALTER TABLE Problem ADD CONSTRAINT FKProblem611514 FOREIGN KEY (AssignedUser) REFERENCES `User` (Id);
-ALTER TABLE Comment ADD CONSTRAINT FKComment537324 FOREIGN KEY (UserId) REFERENCES `User` (Id);
+CREATE TABLE TimeSpent (Id int(10) NOT NULL AUTO_INCREMENT, HoursSpent decimal(2, 0) DEFAULT 0 NOT NULL, Description varchar(512), DateRecorded timestamp DEFAULT NOW() NOT NULL, UserId CHAR(38) NOT NULL, ProblemId int(10) NOT NULL, PRIMARY KEY (Id));
+ALTER TABLE Problem ADD CONSTRAINT FKProblem611514 FOREIGN KEY (AssignedUser) REFERENCES `Users` (Id);
+ALTER TABLE Comment ADD CONSTRAINT FKComment537324 FOREIGN KEY (UserId) REFERENCES `Users` (Id);
 ALTER TABLE Comment ADD CONSTRAINT FKComment818660 FOREIGN KEY (ProblemId) REFERENCES Problem (Id);
 ALTER TABLE Problem_Tag ADD CONSTRAINT FKProblem_Ta147632 FOREIGN KEY (ProblemId) REFERENCES Problem (Id);
 ALTER TABLE Problem_Tag ADD CONSTRAINT FKProblem_Ta741275 FOREIGN KEY (TagId) REFERENCES `Tag` (Id);
-ALTER TABLE TimeSpent ADD CONSTRAINT FKTimeSpent911204 FOREIGN KEY (UserId) REFERENCES `User` (Id);
+ALTER TABLE TimeSpent ADD CONSTRAINT FKTimeSpent911204 FOREIGN KEY (UserId) REFERENCES `Users` (Id);
 ALTER TABLE TimeSpent ADD CONSTRAINT FKTimeSpent807458 FOREIGN KEY (ProblemId) REFERENCES Problem (Id);
 ALTER TABLE Category_Problem ADD CONSTRAINT FKCategory_P899810 FOREIGN KEY (CategoryId) REFERENCES Category (Id);
 ALTER TABLE Category_Problem ADD CONSTRAINT FKCategory_P650586 FOREIGN KEY (ProblemId) REFERENCES Problem (Id);
