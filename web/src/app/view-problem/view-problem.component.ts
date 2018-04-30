90a979dfcd01588bebd3d81839496671943d9f06
tomval <tomovaliuno@gmail.com>
2018-04-30 07:04:48 +0300

    Problem view

--- web/src/app/view-problem/view-problem.component.ts
+++ web/src/app/view-problem/view-problem.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {MatTableDataSource} from '@angular/material';
 @Component({
   selector: 'app-view-problem',
   templateUrl: './view-problem.component.html',
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewProblemComponent implements OnInit {
 
+  displayedColumns = ['position', 'name', 'weight', 'symbol'];
+  dataSource = new MatTableDataSource<Element>(ELEMENT_DATA);
   constructor() { }
 
   ngOnInit() {
   }
 
 }
+
+export interface Element {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: Element[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'}
+];
